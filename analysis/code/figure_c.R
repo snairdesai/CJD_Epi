47c325b4ea00a73c82ca96db07263c59e67d80cc
Sameer Nair-Desai <63830369+snairdesai@users.noreply.github.com>
2023-01-07 13:54:47 -0800

    #5: Revised sample period + removed poor-quality age groups

--- analysis/code/figure_c.R
+++ analysis/code/figure_c.R
@@ -28,16 +28,22 @@ cat("Age-specific national mortality rates by gender have been loaded... \n")
 # Rearranging data for figure.
 fig_c_dat_males <- cleaned_nmr_gender_specific %>%
     dplyr::select(time_period, age_specific_death_rate, gender, age_groups) %>%
-    dplyr::filter(time_period != "1999-2006" &
-                  time_period != "2007 - 2020" &
+    dplyr::filter(time_period != "2007 - 2020" &
                   gender == "Male" &
                   age_groups != "All years" &
                   # Too many missing values in the following age cohorts...
                   age_groups != "25-34 years" &
-                  age_groups != "35-44 years") %>%
+                  age_groups != "35-44 years" &
+                  age_groups != "45-54 years" &
+                  age_groups != "85+ years") %>%
     dplyr::rename(`Gender` = gender, `Age Groups` = age_groups) %>%
     dplyr::mutate(`Year` = as.Date(ISOdate(time_period, 1, 1))) %>%
     tidyr::drop_na()
+
+fig_c_dat_males$`Age Groups` <- ifelse(
+                                fig_c_dat_males$`Age Groups` == "75-84 years",
+                                "75+ years",
+                                fig_c_dat_males$`Age Groups`)
 
 head(fig_c_dat_males)
 
@@ -53,11 +59,7 @@ male_age_specific_deaths_plot <- fig_c_dat_males %>%
                                   "#3288BD")) +
     theme_classic() +
     labs(x = "Year", y = "Male Death Rate by Age Group") +
-    geom_vline(xintercept = as.Date(ISOdate(2007, 1, 1)), linetype = "dotted",
-               color = "black", size = 1.5) +
-   theme(legend.position = "bottom") +
-   annotate(x = as.Date(ISOdate(2007, 1, 1)), y = 425,
-        label = "Switch in ICD Codes", vjust = 2, geom = "label")
+    theme(legend.position = "bottom")
 
 ggsave("output/male_age_specific_deaths_plot.eps")
 ggsave("output/male_age_specific_deaths_plot.pdf")
